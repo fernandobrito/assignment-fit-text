f49a5b6c6d29e3200f98d95427c94ee93587c88a
Fernando Brito <email@fernandobrito.com>
2018-06-02 23:34:33 -0300

    Improve webpack build process

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './reset.css';
+import './base.css';
+
+(window as any).$ = (window as any).jQuery = require('jquery');
+require('jquery-textfill/source/jquery.textfill.js');
+
 import { App } from './components/App';
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
-path = require("path");
+path = require('path');
+HtmlWebpackPlugin = require('html-webpack-plugin');
+CleanWebpackPlugin = require('clean-webpack-plugin');
+MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+basePath = path.resolve(__dirname);
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: {
+    vendor: [
+      'react',
+      'react-dom'
+    ],
+    app: path.resolve(basePath, 'src', 'index.tsx')
+  },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: 'dist/',
+    filename: '[name].bundle.[hash].js',
+    path: path.resolve(basePath, 'dist'),
+    publicPath: '/',
   },
 
-  devServer: {
-    contentBase: [
-      path.resolve(__dirname, "public"),
-      path.resolve(__dirname, "node_modules")
-    ]
-  },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
 
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
 
       {
-        test: /\.scss$/,
+        test: /\.(scss|css|sass)?$/,
         use: [
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          process.env.WEBPACK_MODE !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
@@ -50,12 +55,31 @@ module.exports = {
     ]
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      chunksSortMode: 'dependency'
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  }
 };
